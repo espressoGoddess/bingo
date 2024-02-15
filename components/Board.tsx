a05c989d45fa90fc9e867d705fb8d7d8e5a50530
Amber Shipley <ashipley2@me.com>
2024-02-15 14:11:34 -0800

    Create basic Board and Printable Game components

---
+++ components/Board.tsx
@@ -0,0 +1,25 @@
+import { Task } from './PrintableGame';
+export default function Board({ tasks }: { tasks: Task[] }) {
+	return (
+		<div>
+			<BoardRow tasks={tasks.slice(0, 5)} />
+			<BoardRow tasks={tasks.slice(5, 10)} />
+			<BoardRow center tasks={tasks.slice(10, 14)} />
+			<BoardRow tasks={tasks.slice(14, 19)} />
+			<BoardRow tasks={tasks.slice(19, 24)} />
+		</div>
+	);
+}
+
+function BoardRow({ tasks, center }: { tasks: Task[]; center?: boolean }) {
+	tasks = !center ? tasks : [...tasks.slice(0, 2), { type: 'centerImage' }, ...tasks.slice(2, 4)];
+	const items = tasks.map((task, index) => (
+		<article
+			className={`w-1/5 h-28 text-sm text-gold text-center border-lightGold border m-0.5 rounded-sm flex items-center ${task.type === 'string' ? 'p-2' : ''}`}
+			key={index}
+		>
+			{task.type === 'string' ? task.task : <img src="./bingo-center.png" alt="a + e, andrew and erika" />}
+		</article>
+	));
+	return <div className="flex">{items}</div>;
+}
---
+++ components/PrintableGame.tsx
@@ -0,0 +1,46 @@
+import Image from 'next/image';
+import Board from './Board';
+import { useEffect, useState } from 'react';
+
+export type Task = {
+	type: string;
+	task?: string;
+};
+
+export default function PrintableGame({ tasks }: { tasks: Task[] }) {
+	const [taskCache, setTasks] = useState(tasks);
+	useEffect(() => {
+		setTasks(shuffle(tasks));
+	}, [tasks]);
+	return (
+		<section
+			className="border border-gold border-dotted mx-auto mb-14 pb-16 px-16 hide-border-print flex flex-col justify-around"
+			style={{ width: 723 }}
+		>
+			<header className="text-gold text-center mt-16">
+				<h1 className="text-7xl">VEGAS, BABY!</h1>
+				<div className="flex justify-center items-center h-16  mt-2 mb-14">
+					<div className="w-8 bg-gold h-0.5 mr-2"></div>
+					<h2 className="text-xl">A game of luck and chance</h2>
+					<div className="w-8 bg-gold h-0.5 ml-2"></div>
+				</div>
+			</header>
+			<Board tasks={taskCache} />
+			<footer className="flex w-full justify-center items-center pt-14">
+				<Image alt="poppy flowers outlined in gold" height={89} width={250} src="/footer-flowers.png" />
+			</footer>
+		</section>
+	);
+}
+
+function shuffle(a: any[]) {
+	const b = [...a];
+	var j, x, i;
+	for (i = b.length - 1; i > 0; i--) {
+		j = Math.floor(Math.random() * (i + 1));
+		x = b[i];
+		b[i] = b[j];
+		b[j] = x;
+	}
+	return b;
+}
