61d9fd7f8ddb9c8ccc1924ac20bf10358de14587
Amber Shipley <ashipley2@me.com>
2024-02-20 18:51:26 -0800

    Pull tasks from DB

--- components/Board.tsx
+++ components/Board.tsx
@@ -2,26 +2,30 @@ import Image from 'next/image';
 import centerPhoto from '@/assets/bingo-center.png';
 import { UserTask } from '@/utils/types';
 
-export default function Board({ tasks }: { tasks: Task[] }) {
+export default function Board({ tasks }: { tasks: UserTask[] }) {
+	const orderedTasks = tasks.sort((a, b) => a.grid_row - b.grid_row);
+	console.log('orderedtasks', orderedTasks);
+	//filter by row
 	return (
 		<div>
-			<BoardRow tasks={tasks.slice(0, 5)} />
-			<BoardRow tasks={tasks.slice(5, 10)} />
-			<BoardRow center tasks={tasks.slice(10, 14)} />
-			<BoardRow tasks={tasks.slice(14, 19)} />
-			<BoardRow tasks={tasks.slice(19, 24)} />
+			<BoardRow userTasks={tasks.slice(0, 5)} />
+			<BoardRow userTasks={tasks.slice(5, 10)} />
+			<BoardRow userTasks={tasks.slice(10, 15)} />
+			<BoardRow userTasks={tasks.slice(15, 20)} />
+			<BoardRow userTasks={tasks.slice(20, 25)} />
 		</div>
 	);
 }
 
-function BoardRow({ tasks, center }: { tasks: Task[]; center?: boolean }) {
-	tasks = !center ? tasks : [...tasks.slice(0, 2), { type: 'centerImage' }, ...tasks.slice(2, 4)];
-	const items = tasks.map((task, index) => (
+function BoardRow({ userTasks }: { userTasks: UserTask[] }) {
+	//sort by column
+	const orderedTasks = userTasks.sort((a, b) => a.grid_column - b.grid_column);
+	const items = orderedTasks.map((task, index) => (
 		<article
-			className={`w-1/5 h-28 text-sm text-gold text-center border-lightGold border m-0.5 rounded-sm flex items-center ${task.type === 'string' ? 'p-2' : ''}`}
+			className={`w-1/5 h-28 text-sm text-gold text-center border-lightGold border m-0.5 rounded-sm flex items-center ${task.type !== 'center' ? 'p-2' : ''}`}
 			key={index}
 		>
-			{task.type === 'string' ? task.task : <Image src={centerPhoto} alt="a + e, andrew and erika" />}
+			{task.type !== 'center' ? task.description : <Image src={centerPhoto} alt="a + e, andrew and erika" />}
 		</article>
 	));
 	return <div className="flex">{items}</div>;
