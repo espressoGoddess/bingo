a98d8948603d15c5db7a7b3a504c0bad18021568
Amber Shipley <ashipley2@me.com>
2024-02-21 18:54:48 -0800

    Revert "Direct the user to login instead of pressing a button to login"
    This reverts commit 1e649baee1b9de2636cd89d2a2d7df8d13abc393.

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { signIn, signOut, useSession } from 'next-auth/react';
+
 export default function Component() {
   const { data: session } = useSession();
   if (session) {
@@ -10,8 +11,18 @@ export default function Component() {
       </>
     );
   }
-
-  signIn('google', {
-    callbackUrl: '/print',
-  });
+  return (
+    <>
+      Not signed in <br />
+      <button
+        onClick={() =>
+          signIn('google', {
+            callbackUrl: '/print',
+          })
+        }
+      >
+        Sign in
+      </button>
+    </>
+  );
 }
