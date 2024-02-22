261e38d8e26fb117558b157befc36204cc2a9f55
Amber Shipley <ashipley2@me.com>
2024-02-21 18:00:24 -0800

    Create basic landing page using game secret from URL

---
+++ app/g/[gameSecret]/page.tsx
@@ -0,0 +1,33 @@
+import { createClient } from '@/utils/supabase/server';
+import Link from 'next/link';
+
+export default async function Page({ params }: { params: { gameSecret: string } }) {
+	const supabase = createClient();
+	const { data: game } = await supabase
+		.from('games')
+		.select()
+		.eq('secret', decodeURIComponent(params.gameSecret).toUpperCase());
+
+	if (game?.length) {
+		return (
+			<section className="text-gold text-l mt-28 mx-8">
+				<h1>
+					Welcome to <br />
+					<span className="text-5xl">{game[0].name}</span>
+				</h1>
+				<p className="my-28">
+					A game of <br />
+					<span className="text-5xl">{game[0].tagline.toUpperCase()}</span>
+				</p>
+				<div className="w-full flex justify-center">
+					<Link
+						className="border bg-lightGold text-5xl py-2 px-20 rounded-md border-lightGold bg-opacity-20"
+						href="/login"
+					>
+						Start
+					</Link>
+				</div>
+			</section>
+		);
+	}
+}
