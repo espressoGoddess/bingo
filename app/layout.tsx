4a6da9b99bd5328ead9d0edab0ac0786e55c581d
Amber Shipley <ashipley2@me.com>
2024-02-21 16:10:05 -0800

    Update title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,8 @@ const defaultUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` 
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
+  title: 'A + E Bingo',
+  description: 'Vegas, Baby!',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
