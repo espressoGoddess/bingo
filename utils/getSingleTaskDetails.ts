e46f56720cde4b7a69bc25bd69c4cce115f4b93c
Amber Shipley <ashipley2@me.com>
2024-02-23 11:19:00 -0800

    Create function to fetch a single task with details

---
+++ utils/getSingleTaskDetails.ts
@@ -0,0 +1,28 @@
+import { createClient } from './supabase/server';
+import { UserTask } from './types';
+
+export default async function getUserTasksWithInfo(gameId: number, userId: number, usersTaskId: number) {
+  const supabase = createClient();
+  const { data, error: fetchError } = await supabase
+    .from('users_tasks')
+    .select(`*,task:tasks(description)`)
+    .eq('id', usersTaskId)
+    .eq('user_id', userId)
+    .eq('tasks.game_id', gameId);
+
+  if (fetchError) {
+    throw fetchError;
+  }
+  const singleTaskDetails: UserTask & {
+    task: {
+      description: string;
+    };
+  } = data[0];
+
+  return {
+    description: singleTaskDetails.task.description,
+    completed: singleTaskDetails.completed,
+    grid_row: singleTaskDetails.grid_row,
+    grid_column: singleTaskDetails.grid_column,
+  };
+}
