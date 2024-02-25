24fddef64fef6bcfbbc5ac24dcd4e8c9a27d0377
Amber Shipley <ashipley2@me.com>
2024-02-25 07:30:34 -0800

    Utilize the taskCache state to populate the page instead of props

--- components/TaskDetails.tsx
+++ components/TaskDetails.tsx
@@ -1,14 +1,10 @@
 'use client';
 import { SingleTaskDetails } from '@/utils/types';
 import Link from 'next/link';
+import { useState } from 'react';
 
-export default async function TaskDetails({
-	task,
-	gameSecret,
-}: {
-	task: SingleTaskDetails;
-	gameSecret: string;
-}) {
+export default function TaskDetails({ task, gameSecret }: { task: SingleTaskDetails; gameSecret: string }) {
+	const [taskCache, setTaskCache] = useState(task);
 	const updateStatus = async () => {
 		try {
 			const response = await fetch('/api/updateTaskStatus', {
@@ -16,7 +12,7 @@ export default async function TaskDetails({
 				headers: {
 					'Content-Type': 'application/json',
 				},
-				body: JSON.stringify({ id: task.id, newCompletedStatus: !task.completed }),
+				body: JSON.stringify({ id: task.id, newCompletedStatus: !taskCache.completed }),
 			});
 
 			if (!response.ok) {
@@ -24,25 +20,26 @@ export default async function TaskDetails({
 			}
 
 			const responseData = await response.json();
-			console.log('Updated task:', responseData);
+			setTaskCache(responseData.updatedTask);
 		} catch (error) {
 			console.error('Error updating task:', error);
 		}
 	};
+
 	return (
 		<section className="text-gold mt-28 mx-8">
-			<h1 className="text-5xl">{task.description.toUpperCase()}.</h1>
+			<h1 className="text-5xl">{taskCache.description?.toUpperCase()}.</h1>
 			<div className="flex flex-col items-center">
 				<p className="flex items-center w-1/2 mt-14">
 					Completed:{' '}
-					{task.completed ? (
-						task.completed_at?.toISOString()
+					{taskCache.completed ? (
+						taskCache.completed_at?.toISOString()
 					) : (
 						<span className="w-10 ml-3 border border-gold"></span>
 					)}
 				</p>
 				<Link href="/">Upload Photo</Link>
-				<p onClick={updateStatus}>{!task.completed ? 'Mark Complete' : 'Mark Incomplete'}</p>
+				<p onClick={updateStatus}>{!taskCache.completed ? 'Mark Complete' : 'Mark Incomplete'}</p>
 				<Link href={`/g/${gameSecret}/b`}>Cancel</Link>
 			</div>
 		</section>
