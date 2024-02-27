eb4d6c1a5748a0c3b618f4d8327b6583300fe0ce
Amber Shipley <ashipley2@me.com>
2024-02-26 16:07:20 -0800

    Create addFreeSpaceUserTask helper function

---
+++ utils/addFreeSpaceUserTask.ts
@@ -0,0 +1,27 @@
+import { createClient } from './supabase/server';
+
+export default async function addFreeSpaceUserTask(
+	userId: number,
+	freeSpaceUserTask: string,
+	taskId: number,
+) {
+	const supabase = createClient();
+	console.log(freeSpaceUserTask, 'description inside route');
+	const { data, error: fetchError } = await supabase
+		.from('free_space_user_added_tasks')
+		.insert({ user_id: userId, description: freeSpaceUserTask, user_task_id: taskId })
+		.select();
+
+	if (fetchError) {
+		throw fetchError;
+	}
+	if (!data?.length) {
+		throw new Error('issue adding task');
+	}
+
+	return {
+		user_id: userId,
+		description: freeSpaceUserTask,
+		user_task_id: taskId,
+	};
+}
